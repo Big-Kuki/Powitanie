15446445dd9675be66827276861a39c3ae84ed39
Big-Kuki <daniox2007@gmai.com~>
2026-09-20 17:29:52 +0200

    Zadanie 3 - podpięcie elementów interfejsu przez findViewById

--- app/src/main/java/com/example/powitanie/MainActivity.java
+++ app/src/main/java/com/example/powitanie/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.powitanie;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etName;
+    private Button btnGreet;
+    private TextView tvWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etName = findViewById(R.id.etName);
+        btnGreet = findViewById(R.id.btnGreet);
+        tvWelcome = findViewById(R.id.tvWelcome);
     }
 }
