6b6d8f13b5d456144bd7e45ca89dd661dcac3ed8
Big-Kuki <daniox2007@gmai.com~>
2026-09-20 17:31:25 +0200

    Zadanie 3 - dodanie logiki przycisku, walidacji pustego pola i komunikatu Toast

--- app/src/main/java/com/example/powitanie/MainActivity.java
+++ app/src/main/java/com/example/powitanie/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         etName = findViewById(R.id.etName);
         btnGreet = findViewById(R.id.btnGreet);
         tvWelcome = findViewById(R.id.tvWelcome);
+
+        btnGreet.setOnClickListener(v -> {
+            String name = etName.getText().toString().trim();
+
+            if (name.isEmpty()) {
+                Toast.makeText(MainActivity.this, R.string.toast_empty, Toast.LENGTH_SHORT).show();
+                tvWelcome.setText("");
+            } else {
+                String greeting = getString(R.string.welcome_message, name);
+                tvWelcome.setText(greeting);
+            }
+        });
     }
 }
